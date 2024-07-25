f5f542984f519c5b2d9e4e8fcadf2acb6d26164f
James <dawn147@snu.ac.kr>
2024-07-25 09:49:11 +0900

    Initial Project Setting

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const rootContainer = document.getElementById('root');
+if (rootContainer) {
+  ReactDOM.createRoot(rootContainer).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
