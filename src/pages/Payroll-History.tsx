b99ec28f29bb41cb45d9726f65be4cdb60326d66
James <dawn147@snu.ac.kr>
2024-07-26 15:45:26 +0900

    feat: 임시 페이지 구현 (#5)

---
+++ src/pages/Payroll-History.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PayrollHistory: React.FC = () => (
+  <div>
+    <h1>PayRoll</h1>
+  </div>
+);
+
+export default PayrollHistory;
---
+++ src/pages/Schedule-Manage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ScheduleManage: React.FC = () => (
+  <div>
+    <h1>Schedule Manage</h1>
+  </div>
+);
+
+export default ScheduleManage;
