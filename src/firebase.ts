05182148bb4b303a2a73c6eef2314fa815d7aae2
ssumanlife <ksm9805@naver.com>
2024-07-29 11:34:37 +0900

    firebase넣기,읽기customHook

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,36 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-// import { getDatabase } from 'firebase/firestore';
+// import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface ImportMeta {
+    readonly env: {
+      readonly VITE_FIREBASE_DATABASE_URL: string;
+      readonly VITE_FIREBASE_API_KEY: string;
+      readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+      readonly VITE_FIREBASE_PROJECT_ID: string;
+      readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+      readonly VITE_FIREBASE_MESSAGING_ID: string;
+      readonly VITE_FIREBASE_APP_ID: string;
+      readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+    };
+  }
+}
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
-// const fireStore = getDatabase(firebase);
+const db = getFirestore(firebase);
 const authService = getAuth();
-// connectFirestoreEmulator(fireStore, 'localhost', 5173);
 
-export { authService, firebase };
+export { authService, db };
--- src/pages/Payroll-History.tsx
+++ src/pages/Payroll-History.tsx
@@ -1,45 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { ref } from 'firebase/database';
+import React from 'react';
 
-const PayrollHistory: React.FC = () => {
-  const [todo, setTodo] = useState('');
-
-  const handleTodoChange = (e: any) => {
-    setTodo(e.target.value);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-  async function getData() {
-    const url = `https://workcheck-d7768-default-rtdb.asia-southeast1.firebasedatabase.app/`;
-    // const json = JSON.stringify(url)
-    try {
-      const data = await axios.get(JSON.stringify(url));
-      console.log(data);
-      if (data) {
-        setTodo(data);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  // Write
-  // const writeData = () => {
-  //   const name: string = 'sumin';
-  //   set(ref(firebase, `test/${name}`), {
-  //     todo,
-  //     name,
-  //   });
-  //   setTodo('');
-  // };
-
-  return (
-    <div className="App">
-      <input type="text" value={todo} onChange={handleTodoChange} />
-      {/* <button onClick={writeData}>Submit</button> */}
-    </div>
-  );
-};
+const PayrollHistory: React.FC = () => (
+  <div>
+    <h1>PayRoll</h1>
+  </div>
+);
 
 export default PayrollHistory;
