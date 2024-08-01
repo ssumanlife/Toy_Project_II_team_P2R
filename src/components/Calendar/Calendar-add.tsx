371d40aff6cd5f63bb71d00293ff0f54d828eeb8
miniseungmin <liontmdals@gmail.com>
2024-08-01 20:33:25 +0900

    feat:이벤트 추가모달 유효성검사(34)

--- src/components/Calendar/Calendar-add.tsx
+++ src/components/Calendar/Calendar-add.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import Modal from '../Modal';
 import Button from '../Button';
@@ -20,22 +20,49 @@ const CalendarAddModal = ({ isOpen, onClose, onAddEvent }) => {
   const [startDateTime, setStartDateTime] = useState('');
   const [endDateTime, setEndDateTime] = useState('');
   const [selectedColor, setSelectedColor] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const resetForm = () => {
+    setTitle('');
+    setStartDateTime('');
+    setEndDateTime('');
+    setSelectedColor('');
+    setErrorMessage('');
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      resetForm();
+    }
+  }, [isOpen]);
+
+  const handleClose = () => {
+    resetForm();
+    onClose();
+  };
 
   const handleSubmit = () => {
-    if (title && startDateTime && endDateTime) {
-      const newEvent = {
-        title,
-        start: startDateTime,
-        end: endDateTime,
-        category: selectedColor,
-      };
-      onAddEvent(newEvent);
-      onClose();
-      setTitle('');
-      setStartDateTime('');
-      setEndDateTime('');
-      setSelectedColor('');
+    if (!title || !startDateTime || !endDateTime) {
+      setErrorMessage('모든 필드를 입력해주세요');
+      return;
     }
+
+    const startDate = new Date(startDateTime);
+    const endDate = new Date(endDateTime);
+
+    if (startDate > endDate) {
+      setErrorMessage('일정시작이 일정종료 보다 늦을 수 없습니다.');
+      return;
+    }
+
+    const newEvent = {
+      title,
+      start: startDateTime,
+      end: endDateTime,
+      category: selectedColor,
+    };
+    onAddEvent(newEvent);
+    handleClose();
   };
 
   return (
@@ -87,6 +114,7 @@ const CalendarAddModal = ({ isOpen, onClose, onAddEvent }) => {
             ))}
           </div>
         </div>
+        {errorMessage && <div css={errorMessageStyle}>{errorMessage}</div>}
         <div css={buttonContainerStyle}>
           <Button onClick={handleSubmit}>작성</Button>
         </div>
@@ -184,4 +212,9 @@ const titleStyle = css`
   display: block;
   border-bottom: 1px solid var(--text-white-gray);
   padding-bottom: 20px;
+`;
+const errorMessageStyle = css`
+  color: red;
+  margin-top: 30px;
+  font-size: 1.5em;
 `;
