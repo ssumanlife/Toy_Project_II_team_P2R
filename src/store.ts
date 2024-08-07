926c8ff02ca86ec6bf70b214e8dbcfb94431e338
miniseungmin <liontmdals@gmail.com>
2024-08-07 22:03:25 +0900

    docs: store.ts 중복선언 삭제(56)

--- src/store.ts
+++ src/store.ts
@@ -11,7 +11,6 @@ const store = configureStore({
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
