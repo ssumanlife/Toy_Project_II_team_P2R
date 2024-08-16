552d13bbd8761a8a07f2fa1a5270ec85acc7eb06
ssumanlife <ksm9805@naver.com>
2024-08-16 11:55:30 +0900

    refactor: firebaseAPI코드수정

--- src/API/Firebase/UpdatePayrollCorApp.ts
+++ src/API/Firebase/UpdatePayrollCorApp.ts
@@ -1,32 +1,25 @@
-import { collection, doc, setDoc, getDocs } from 'firebase/firestore';
+import { collection, doc, setDoc, getDocs, query, where } from 'firebase/firestore';
 import { db } from './Firebase_Config.ts';
 
 const updateCorrectionState = async (name: string, month: number, state: string, correctionDetails: string) => {
   try {
     const membersSnapshot = await getDocs(collection(db, 'members'));
-
-    const memberPromises = membersSnapshot.docs.map(async (memberDoc) => {
-      const collectionSnapshot = await getDocs(collection(db, `members/${memberDoc.id}/payrollCorApp`));
-
-      const payDataPromises = collectionSnapshot.docs.map(async (payDataDoc) => {
-        const payDataId = payDataDoc.id;
-        const payData = payDataDoc.data();
-
-        if (payData.name === name && payData.month === month && payData.correctionDetails === correctionDetails) {
-          await setDoc(doc(db, `members/${memberDoc.id}/payrollCorApp`, payDataId), {
-            name: payData.name,
-            month: payData.month,
-            correctionState: state,
-            correctionDetails: payData.correctionDetails,
-            reasonForApplication: payData.reasonForApplication,
-          });
-        }
+    const payrollCorAppUpdatePromise = membersSnapshot.docs.map(async (memberDoc) => {
+      const q = query(
+        collection(db, 'members', memberDoc.id, 'payrollCorApp'),
+        where('name', '==', name),
+        where('month', '==', month),
+        where('correctionDetails', '==', correctionDetails),
+      );
+      const querySnapshot = await getDocs(q);
+      return querySnapshot.docs.map(async (docSnapshot) => {
+        setDoc(doc(db, 'members', memberDoc.id, 'payrollCorApp', docSnapshot.id), {
+          ...docSnapshot.data(),
+          correctionState: state,
+        });
       });
-
-      await Promise.all(payDataPromises);
     });
-
-    await Promise.all(memberPromises);
+    await Promise.all(payrollCorAppUpdatePromise);
   } catch (error) {
     console.error('Error updating correction state:', error);
   }
