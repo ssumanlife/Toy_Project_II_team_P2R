787c90ae07b3e69cdd2b7f123089f77c87d47157
James <dawn147@snu.ac.kr>
2024-08-18 18:23:56 +0900

    캐시된 파일들 삭제

--- src/pages/Schedule-Manage/Schedule-Manage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MyCalendar from '../../Components/Calendar/Calendar.tsx';
-
-const ScheduleManage: React.FC = () => <MyCalendar />;
-
-export default ScheduleManage;
